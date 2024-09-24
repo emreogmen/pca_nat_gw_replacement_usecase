3de2bf13db3f7129ba366ad1325fa6ee40d74b41
Paul Carvill <85612526+Eskimoodigital@users.noreply.github.com>
2024-09-24 14:39:46 +0200

    final comments

--- main.tf
+++ main.tf
@@ -15,6 +15,21 @@
 ###################################################################################################################################################################################################
 ###################################################################################################################################################################################################
 
+### This code will deploy a VPC in AWS with 2 Linux workloads running Gatus
+### It can also deploy an Aviatrix Spoke gateway for egress if you choose to (default is to deploy)
+###
+### Before running, please note the following:
+###
+### 1. Update the AWS Provider with your credentials information
+### 2. Update the tfvars file to your required inputs if you choose to use tfvars
+### 3. You can modify the Gatus config in the vpc1_test_server.tftpl file if you so wish (recommend to use it as is)
+### 4. If you run without tfvars, you will be prompted for your AWS account name, controller ip and credentials
+### 5. The code will output the loadbalancer URL for the 2 workloads. It's the same URL with port 80 and port 81
+###
+
+###################################################################################################################################################################################################
+###################################################################################################################################################################################################
+###################################################################################################################################################################################################
 
 
 terraform {
@@ -34,8 +49,6 @@ terraform {
 }
 
 
-
-#change
 
 provider "aviatrix" {
   skip_version_validation = true
