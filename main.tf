1b4be930e042272fe9f9beec38a4eb6f23aae038
emreogmen <ogmen.emre@gmail.com>
2024-09-27 09:51:53 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ provider "aws" {
   region = var.aws_region
   # shared_credentials_files = ["~/.aws/credentials"]
   # profile = "SubAccountAdmin-535708457972"
-  profile = var.aws_profile  # Use the aws_profile variable here
+  # profile = var.aws_profile  # Use the aws_profile variable here
   #access_key = var.aws_access_key
   #secret_key = var.aws_secret_key
 }
