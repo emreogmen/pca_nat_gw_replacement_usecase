ede3ffbf11a07492ed1eab48bf352c6a2a3f930f
emreogmen <ogmen.emre@gmail.com>
2024-09-27 09:36:00 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -43,9 +43,21 @@ variable "aws_profile" {
   type        = string
 }
 
-variable "aws_credentials_path" {
-  description = ".aws/credentials"
-  default     = "~/.aws/credentials"
+#variable "aws_credentials_path" {
+#  description = ".aws/credentials"
+#  default     = "~/.aws/credentials"
+#}
+
+variable "aws_access_key" {
+  description = "AWS access key for authentication"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key for authentication"
+  type        = string
+  sensitive   = true
 }
 
 
